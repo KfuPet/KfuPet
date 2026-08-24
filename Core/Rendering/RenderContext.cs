3bdfdd0684b15499b4902f09c9b15786f9adcb3c
Lr灰弹 <l2179725762@gmail.com>
2026-08-24 23:11:48 +0800

    fix：优化无用代码
    一不小心又发现了远古时期的屎山

--- Core/Rendering/RenderContext.cs
+++ Core/Rendering/RenderContext.cs
@@ -1,4 +1,3 @@
-using System.Windows;
 using System.Windows.Controls;
 
 namespace KfuPet.Core.Rendering
@@ -7,28 +6,9 @@ namespace KfuPet.Core.Rendering
     {
         public Canvas Canvas { get; }
 
-        public double DpiScaleX { get; set; } = 1.0;
-
-        public double DpiScaleY { get; set; } = 1.0;
-
         public RenderContext(Canvas canvas)
         {
             Canvas = canvas;
-        }
-
-        public void UpdateDpiScale()
-        {
-            var source = PresentationSource.FromVisual(Canvas);
-            if (source?.CompositionTarget != null)
-            {
-                DpiScaleX = source.CompositionTarget.TransformFromDevice.M11;
-                DpiScaleY = source.CompositionTarget.TransformFromDevice.M22;
-            }
-            else
-            {
-                DpiScaleX = 1.0;
-                DpiScaleY = 1.0;
-            }
         }
     }
 }
